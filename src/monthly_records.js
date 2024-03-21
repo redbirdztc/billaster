ea5456100f75c00a15b8bf77b04b5826be44390d
redbirdztc <redbirdztc@gmail.com>
2024-03-21 10:10:43 +0800

    feat: sort records by date in monthly records

--- src/monthly_records.js
+++ src/monthly_records.js
@@ -50,30 +50,55 @@ const MonthlyRecords = ({ records }) => {
             unit: "Yuan",
             isExpense: false
         },
+        {
+            date: new Date(new Date().setDate(new Date().getDate() + 1)),
+            category: {
+                "icon": FoodIcon,
+                "code": "Food"
+            },
+            account: {
+                "icon": PiggyBank,
+                "code": "Savings"
+            },
+            amount: 100,
+            unit: "Yuan",
+            isExpense: false
+        },
     ]
 
     // records 按照日期分组并按照日期倒序排序
     const recordsByDate = records.reduce((acc, record) => {
-        const date = record.date;
+        const date = record.date.getDate();
         if (!acc[date]) {
             acc[date] = [];
         }
         acc[date].push(record);
         return acc;
     }, {});
+
+    const daily = [];
+    Object.keys(recordsByDate).forEach(date => {
+        daily.push({ "date": date, "data": recordsByDate[date] });
+    });
+
+    console.log(daily)
+    // sort daily by date
+    daily.sort((a, b) => {
+        return b.date - a.date;
+    });
+
     return (
-         <div className="col-broker" style={{width:'90vw', height: "90vh", alignItems: 'center',marginLeft:'5vw',marginRight:'5vw' }}>
+        <div className="col-broker" style={{ width: '90vw', height: "90vh", alignItems: 'center', marginLeft: '5vw', marginRight: '5vw' }}>
             {
-                Object.keys(recordsByDate).map((date, index) => {
+                daily.map((daily, index) => {
                     return (
-                        <div key={index}  style={{width:'100%'}} >
-                            {DailyRecords({ date: recordsByDate[date][0].date, records: recordsByDate[date] })}
+                        <div key={index} style={{ width: '100%' }} >
+                            {DailyRecords({ date: daily.data[0].date, records: daily.data })}
                         </div>
                     )
                 })
             }
         </div>
-
     )
 }
 
